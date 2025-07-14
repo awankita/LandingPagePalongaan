f2f39d816bf6d247b17e65ff38a722e6fe93a002
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2025-07-14 14:01:25 +0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //if you looking for annoying fuckers on left bottom corner, uncomment this
+  
+  // devIndicators: {
+  //   buildActivity: false,
+  //   buildActivityPosition: 'bottom-right',
+  // },
 };
 
 export default nextConfig;
