45222d446fcafa6e869bc02cea2ef7cb2a874537
MayCleanSitepu <113108027+MayCleanSitepu@users.noreply.github.com>
2025-08-03 13:09:25 +0700

    fix: url stream cam

--- app/cctvs/cctvs.ts
+++ app/cctvs/cctvs.ts
@@ -1,21 +1,21 @@
 const cctvs = [
   {
-    name: "CCTV Tugu Masuk Palongaan",
-    address: "Tugu Masuk Palongaan",
+    name: "CCTV A",
+    address: "...",
     device: "C01-Awankita",
-    ip: "https://cctv.awankita.id/hls/cam3/stream.m3u8",
+    ip: "https://cctv.awankita.id/hls/cam1/stream.m3u8",
   },
   {
     name: "CCTV B",
     address: "....",
     device: "C02-Awankita",
-    ip: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    ip: "https://cctv.awankita.id/hls/cam2/stream.m3u8",
   },
   {
     name: "CCTV C",
     address: "....",
     device: "C03-Awankita",
-    ip: "https://stream-fastly.castr.com/5b9352dbda7b8c769937e459/live_2361c920455111ea85db6911fe397b9e/index.fmp4.m3u8",
+    ip: "https://cctv.awankita.id/hls/cam3/stream.m3u8",
   },
 ];
 export default cctvs;
